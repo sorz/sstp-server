baf6d2c3c5893286526a56b0a4674cbc24de8b53
sorz <orz@sorz.org>
2015-11-09 22:26:03 +0800

    Add PPP codec test

---
+++ tests/pppd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+from sys import stdin, stdout
+from tty import setcbreak, TCSANOW
+import time
+
+
+LCP1_EN = (b'\x7e\xff\x7d\x23\xc0\x21\x7d\x24\x7d\x20\x7d\x20\x7d\x27\x7d\x2d'
+           b'\x7d\x23\x7d\x26\xad\x36\x7e')
+IP1_EN = (b'\x7e\x80\x21\x7d\x22\x7d\x22\x7d\x20\x7d\x2a\x7d\x23\x7d\x26\x7d'
+          b'\x2a\x7d\x2a\x20\x7d\x21\x6d\xf9\x7e')
+
+def main():
+    setcbreak(stdin, TCSANOW)
+
+    stdout.write(LCP1_EN + LCP1_EN[1:])
+    stdout.flush()
+    assert stdin.read(len(LCP1_EN)) == LCP1_EN
+    assert stdin.read(len(LCP1_EN)) == LCP1_EN
+
+    time.sleep(0.2)  # waiting for auth ok
+
+    stdout.write(IP1_EN)
+    stdout.flush()
+    assert stdin.read(len(IP1_EN)) == IP1_EN
+    stdout.write(IP1_EN)
+    stdout.flush()
+
+    time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    main()
+
--- tests/test.py
+++ tests/test.py
@@ -6,8 +6,14 @@ import ssl
 
 
 CERT = 'tests/self-signed.pem'
-ARGS = ['sstpd', '-c', CERT, '-p', '4433',
-        '--pppd-config', 'tests/options.sstpd']
+ARGS = ['sstpd', '-c', CERT, '-p', '4433', '-v', '5',
+        '--pppd', 'tests/pppd.py']
+
+CERT_HASH = (b'\x9f\xf0\xa8\x8c\xa0\x9c\x00\x6e\x0f\xb0\x22\x2e\xfa\xb6\x5f'
+             b'\x4c\xf3\xf5\xb2\x15\xfb\xd2\x6b\x83\x26\x72\x6d\xc6\x88\x12'
+             b'\x61\x15')
+LCP1_DE = (b'\xff\x03\xc0\x21\x04\x00\x00\x07\x0d\x03\x06')
+IP1_DE = (b'\x80\x21\x02\x02\x00\x0a\x03\x06\x0a\x0a\x20\x01')
 
 def _ssl_connect():
     conn = socket.create_connection(('127.0.0.1', 4433))
@@ -33,20 +39,75 @@ def _sstp_handshake(conn):
     time.sleep(0.1)
 
     # SSTP_MSG_CALL_CONNECT_ACK
-    assert conn.read(4) == b'\x10\x01\x00\x30'  # ver, C, len
-    assert conn.read(4) == b'\x00\x02\x00\x01'  # type, num attr
-    assert conn.read(4) == b'\x00\x04\x00\x28'  # attr 1
-    assert conn.read(4) == b'\x00\x00\x00\x03'  # proto bitmask
-    nonce = conn.read(32)
+    assert f.read(4) == b'\x10\x01\x00\x30'  # ver, C, len
+    assert f.read(4) == b'\x00\x02\x00\x01'  # type, num attr
+    assert f.read(4) == b'\x00\x04\x00\x28'  # attr 1
+    assert f.read(4) == b'\x00\x00\x00\x03'  # proto bitmask
+    nonce = f.read(32)
     assert len(nonce) == 32
+    f.close()
     return nonce
+
+
+def _ppp_lcp_test(conn):
+    f = conn.makefile('rwb')
+
+    assert f.read(4) == b'\x10\x00\x00\x0f'
+    assert f.read(len(LCP1_DE)) == LCP1_DE
+    assert f.read(4) == b'\x10\x00\x00\x0f'
+    assert f.read(len(LCP1_DE)) == LCP1_DE
+
+    f.write(b'\x10\x00\x00\x0f')
+    f.write(LCP1_DE)
+    f.write(b'\x10\x00\x00\x0f')
+    f.write(LCP1_DE)
+    f.flush()
+
+    # Drop echos
+    #f.read((4 + len(LCP1_DE)) * 2)
+
+    f.close()
+
+
+def _ppp_ip_test(conn):
+    f = conn.makefile('rwb')
+    assert f.read(4) == b'\x10\x00\x00\x10'
+    assert f.read(len(IP1_DE)) == IP1_DE
+
+    f.write(b'\x10\x00\x00\x10')
+    f.write(IP1_DE)
+    f.flush()
+
+    assert f.read(4) == b'\x10\x00\x00\x10'
+    assert f.read(len(IP1_DE)) == IP1_DE
+
+
+def _sstp_connected(conn, nonce):
+    f = conn.makefile('rwb')
+
+    # SSTP_MSG_CALL_CONNECTED
+    f.write(b'\x10\x01\x00\x70\x00\x04')  # ver, C, len, type
+    f.write(b'\x00\x01\x00\x03\x00\x68')  # attr
+    f.write(b'\x00\x00\x00\x02')  # hash bitmap
+    f.write(nonce)
+    f.write(CERT_HASH)
+    f.write(b'\x00' * 32)  # MAC
+    f.flush()
+    f.close()
 
 
 def test_connect():
     conn = _ssl_connect()
+    time.sleep(0.1)
     _http_handshake(conn)
-    _sstp_handshake(conn)
-    # TODO: PPP handshake
+    time.sleep(0.1)
+    nonce = _sstp_handshake(conn)
+    time.sleep(0.1)
+    _ppp_lcp_test(conn)
+    time.sleep(0.1)
+    _sstp_connected(conn, nonce)
+    time.sleep(0.1)
+    _ppp_ip_test(conn)
 
 
 def main():
@@ -55,7 +116,7 @@ def main():
     try:
         test_connect()
     finally:
-        time.sleep(3)
+        time.sleep(1)
         process.terminate()
 
 
